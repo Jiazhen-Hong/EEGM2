0426420260bb2d99ee8ba79dd64d0f15025a2637
jiazhen@emotiv.com <jiazhen@emotiv.com>
2025-02-24 16:49:21 -0800

    .gitignore update

--- code/Github_models/BIOT_models/__init__.py
+++
@@ -1,8 +0,0 @@
-#copy from https://github.com/ycq091044/BIOT/blob/main
-#date 2025-02-06
-from .sparcnet import SPaRCNet
-from .contrawr import ContraWR
-from .cnn_transformer import CNNTransformer
-#from .ffcl import FFCL
-from .st_transformer import STTransformer
-from .biot import BIOTClassifier, UnsupervisedPretrain, SupervisedPretrain, BIOTEncoder
--- code/Github_models/EEGPT_models/__init__.py
+++
@@ -1,3 +0,0 @@
-#copy from https://github.com/ycq091044/BIOT/blob/main
-#modified by Jiazhen, to avoid data loading. As we use this for memory measurement only
-#date 2025-02-06
